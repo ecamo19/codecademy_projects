be9d3a8188d80d6a43d64b10ba73a7be313e60fd
ecamo19 <erickcamo19@gmail.com>
2021-07-17 12:02:43 -0400

    updating functions

---
+++ functions/.ipynb_checkpoints/count_colors-checkpoint.py
@@ -0,0 +1,71 @@
+import argh
+import numpy as np
+
+
+# Count number of colors in image -----------------------------------------------
+
+def count_colors(image):
+    """
+    This function was developed using this repository:
+    https://github.com/townsean/pixel-color-count/blob/master/pixel-color-count.py
+    by Ashley Grenon https://github.com/townsean
+    
+    The function register the RGB values (eg: 0,0,4) of each single pixel.
+    A color is define here a combination of the three posible combinations of
+    RGB values.
+    
+    For example if one pixel has a value of [0,0,0] this is register as a color 
+    while a pixel with a value of  [0,0,1] is register as a new color
+    
+    """
+    
+    # Get dimmension of the image 
+    width, height = image.size
+    
+    # Covert image to RGB
+    rgb_image = image.convert('RGB')
+    
+    # Create empty dictinary to store the RGB
+    color_count = {}
+    
+    # Iterate through each single pixel in the image and keep a count of the 
+    # RGB values of each single pixel
+    
+    for each_column in range(width):
+        for each_row in range(height):
+            
+            # Get RGB value
+            rgb = rgb_image.getpixel((each_column, each_row))
+            
+            # Add it to the dictionary
+            if rgb in color_count:
+                color_count[rgb] += 1
+            else:
+                color_count[rgb] = 1
+    
+    # Print the total number of colours.
+    print(f'\n> Your image has {len(color_count.keys())} different colors\n')
+
+
+if __name__ == '__main__':
+    argh.dispatch_command(count_colors)
+    
+
+# Test function -----------------------------------------------------------------
+# Load packages
+# import cv2
+# from PIL import Image
+# import numpy as np
+# import argh
+
+# path_to_image = '/home/ecamo19/Documents/cursos_libros_tutoriales/cursos/codeacademy/projects/cs101_final_project/test_images/bird.png'
+#          
+# # Read image PIL, here the image is not an array
+# image = Image.open(path_to_image)
+# 
+# count_colors(image)
+
+
+
+
+
---
+++ functions/count_and_reduce_program.py
@@ -0,0 +1,109 @@
+# Packages ----------------------------------------------------------------------
+import cv2
+from PIL import Image
+import numpy as np
+import glob
+import os
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+# Load selfmade functions
+from functions.count_colors import *
+from functions.color_reduction_k_means import *
+
+def count_and_reduce():
+    """
+    This function contains all for the color count and color reduction
+    """
+
+    print("\nPlease,enter the directory where your images are stored")
+    print("...or enter the word test to analize test images included ")
+    print("\npath example: '/path/to/'")
+    print("\tFor quitting the program enter quit.")
+
+    flag = True
+    while True:
+
+    # Get image path from user -------------------------------------------------
+        path_to_image = input("\n Where are your images stored? or enter the word test: ")
+        if path_to_image == "quit":
+            flag = False
+            print("\n\tBye!\n")
+            break   
+
+        elif path_to_image == "test":
+            path_to_image = "../image_color_count_and_color_reduction/test_images"
+
+        # Read images available in the folder ----------------------------------
+        print("\nImages available: ")
+
+        for each_image in os.listdir(path_to_image):
+
+            if each_image.endswith('png') or each_image.endswith('jpg'):        
+                print(f'\t> {os.path.basename(each_image)}')
+
+        # Choose image ---------------------------------------------------------
+        image_name = input('\nWhich image do you want to analize?: ')
+        if image_name == "quit":
+            flag = False
+            print("\n\tBye!\n")
+            break  
+
+        else:
+            # Read images using PIL --------------------------------------------
+            # image is not an array
+            image = Image.open(f'{path_to_image}/{image_name}') 
+
+            # Image for kmeans, convert image to array
+            img_kmeans = np.array(image)
+
+            # Count colors function --------------------------------------------
+
+            count_colors(image)
+
+            # Reduce colors ----------------------------------------------------
+            print("\nDo you want to reduce the colors of your image:")
+            quantization = input("yes or no: ")
+
+            if quantization != "yes":
+                    flag = False
+                    print("\n\tBye!\n")
+                    break 
+
+            else:
+
+                # Color reduction
+                number_of_colors = int(input("How many colors do you want to display: "))
+                clustered_image = reduce_colors(img_kmeans, number_of_colors = number_of_colors)
+
+                # Concatanate images horizontally
+                horizontal_images = np.concatenate((image, clustered_image), axis=1)
+                figure(figsize=(20, 15), dpi = 300)
+
+                fig = plt.imshow(horizontal_images)
+
+                fig.axes.get_xaxis().set_visible(False)
+                fig.axes.get_yaxis().set_visible(False)
+
+                plt.show()
+
+                print("\nDo you want to save your new image:")
+                save_image = input("yes or no: ")
+
+                if save_image == "no":
+                    continue
+
+                elif save_image == "yes":
+                    print("\tPlease specify where the image should be saved")
+                    print("\tPath example: '/path/to/save'")
+                    path_to_save = input("\nPath: ")
+                    image_name = input("\nImage name: ")
+                    cv2.imwrite(f'{path_to_save}/{image_name}.jpg',cv2.cvtColor(clustered_image,                               cv2.COLOR_RGB2BGR))
+                    cv2.waitKey(0)
+                    print("\n> Image saved!")
+                    flag = False
+                    print("\n\tBye!\n")
+                    break 
+
+if __name__ == '__main__':
+    argh.dispatch_command(count_and_reduce)
